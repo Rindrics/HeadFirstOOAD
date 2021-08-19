b83b0848243a7cc1a8e533384e76bc2283c6591c
Akira <smxshxishxad@me.com>
2021-08-19 23:13:19 +0900

    :tada: Add BarkRecognizer

---
+++ chap3/BarkRecognizer.java
@@ -0,0 +1,11 @@
+public class BarkRecognizer {
+
+    private DogDoor door;
+
+    public BarkRecognizer(DogDoor door) {
+        this.door = door;
+    }
+
+    public void recognize(Bark bark) {
+    }
+}
