48dabfb667baf6aa9417b7a4058fbea48624c872
Akira <smxshxishxad@me.com>
2021-08-20 00:31:37 +0900

    :sparkles: Close door automatically by time

--- chap3/DogDoor.java
+++ chap3/DogDoor.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DogDoor {
 
@@ -14,6 +16,14 @@ public class DogDoor {
     public void open() {
         System.out.println("The dog door opens.");
         open = true;
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+                public void run() {
+                    close();
+                    timer.cancel();
+                }
+            }, 5000);
     }
 
     public void close() {
