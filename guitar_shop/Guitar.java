f19e2097c4112775fcc2eb02521ad9b4332c971b
Akira <smxshxishxad@me.com>
2021-08-21 00:03:01 +0900

    :recycle: Extend Guitar from Instrument

--- guitar_shop/Guitar.java
+++ guitar_shop/Guitar.java
@@ -1,43 +1,15 @@
-public class Guitar {
+public class Guitar extends Instrument {
     private String serialNumber, model;
     private Builder builder;
-    private Type type;
-    private Wood backWood, topWood;
-    private double price;
+    private InstrumentSpec spec;
 
     public Guitar(String serialNumber, double price,
-                  Builder builder, String model, Type type,
-                  Wood backWood, Wood topWood) {
+                  InstrumentSpec spec) {
         this.serialNumber = serialNumber;
         this.price = price;
-        this.builder = builder;
-        this.model = model;
-        this.type = type;
-        this.backWood = backWood;
-        this.topWood = topWood;
-    }
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-    public double getPrice() {
-        return price;
+        this.spec = spec;
     }
     public void setPrice(double newPrice) {
         this.price = newPrice;
-    }
-    public Builder getBuilder() {
-        return builder;
-    }
-    public String getModel() {
-        return model;
-    }
-    public Type getType() {
-        return type;
-    }
-    public Wood getBackWood() {
-        return backWood;
-    }
-    public Wood getTopWood() {
-        return topWood;
     }
 }
--- guitar_shop/Instrument.java
+++ guitar_shop/Instrument.java
@@ -1,14 +1,22 @@
 public class Instrument {
     public String serialNumber;
     public double price;
+    public InstrumentSpec spec;
 
     public Instrument(String serialNumber, double price, InstrumentSpec spec) {
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.spec = spec;
     }
 
-    public InstrumentSpec getSpec() {
+    public String getSerialNumber() {
+        return serialNumber;
     }
 
     public double getPrice() {
         return price;
+    }
+
+    public InstrumentSpec getSpec() {
     }
 }
