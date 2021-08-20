f5823ce4f5ffc41004d0eabf3e58021030455866
Akira <smxshxishxad@me.com>
2021-08-20 23:35:30 +0900

    :sparkles: Add .getProperty() and .getProperties() to InstrumentSpec

--- guitar_shop/InstrumentSpec.java
+++ guitar_shop/InstrumentSpec.java
@@ -2,7 +2,15 @@ import java.util.Map;
 
 public class InstrumentSpec {
     public String model;
+    private Map properties;
 
     public InstrumentSpec(Map properties) {
+    }
+
+    public String getProperty(String propertyName) {
+    }
+
+    public Map getProperties() {
+        return properties;
     }
 }
